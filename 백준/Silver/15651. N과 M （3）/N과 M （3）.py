42175d03162f65f164bd50ae3faa80c9170a4df1
Sicoree <97591071+Sicoree@users.noreply.github.com>
2023-05-12 19:42:39 +0900

    [Silver III] Title: N과 M (3), Time: 1376 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,12 +1,13 @@
-def DFS(r):
+def DFS():
     if len(r) == M:
         print(' '.join(map(str, r)))
         return
     
     for i in range(1, N + 1):
         r.append(i)
-        DFS(r)
+        DFS()
         r.pop()
 
 N, M = map(int, input().split())
-DFS([])
+r = []
+DFS()
