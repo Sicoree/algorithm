055a99ec7ae245912a7085e41547be29398a7484
Sicoree <97591071+Sicoree@users.noreply.github.com>
2023-12-04 02:59:16 +0900

    [Gold IV] Title: 알파벳, Time: 5156 ms, Memory: 163720 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,37 +1,27 @@
-import sys
-input = sys.stdin.readline
-
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
-def dfs(x, y, cnt):
+def DFS(cx, cy, cnt):
     global result
     if result < cnt:
-        result = cnt
-        
-    for idx in range(4):
-        nx = x + dx[idx]
-        ny = y + dy[idx]
+        result = cnt 
 
-        if 0 <= nx < R and 0 <= ny < C and visited[arr[nx][ny]] == 0:
-            # visited.append(arr[nx][ny])
-            # dfs(nx, ny, cnt + 1)
-            # visited.pop()
+    for idx in range(4):
+        nx = cx + dx[idx]
+        ny = cy + dy[idx]
+
+        if 0 <= nx < N and 0 <= ny < M and visited[arr[nx][ny]] == 0:
             visited[arr[nx][ny]] = 1
-            dfs(nx, ny, cnt + 1)
+            DFS(nx, ny, cnt + 1)
             visited[arr[nx][ny]] = 0
 
-R, C = map(int, input().split())
-# arr = [list(input().rstrip()) for _ in range(R)]
-# visited = []
-# visited.append(arr[0][0])
-
+N, M = map(int, input().split())
 # 아스키 코드로 바꿔준다고 함
-arr = [list(map(lambda x : ord(x) - 65, input())) for _ in range(R)]
+arr = [list(map(lambda x : ord(x) - 65, input())) for _ in range(N)]
 visited = [0] * 26
 visited[arr[0][0]] = 1
 
-result = 1
-dfs(0, 0, result)
+result = 0
 
+DFS(0, 0, 1)
 print(result)
